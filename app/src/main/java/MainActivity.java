32a8578bc9af5f1506f71b403ec53202ef1d9d15
Daria <dasharil1997@yandex.ru>
2019-10-23 20:37:54 +0300

    *

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,14 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);//будет отоюражать вид
+    }
+
+}
